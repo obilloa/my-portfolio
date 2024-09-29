52c216c81f3d8df98c9bd309ab533405cd4d9e7b
obilloa <obilloanthony07@gmail.com>
2024-09-29 13:27:35 +0800

    29/09/2024 - Updated Background color

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+  
 }
